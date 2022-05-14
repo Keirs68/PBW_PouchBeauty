47ba24c5ee0544c18675daba487c941d2d31d8f0
Putri Maulida <PUTRI.M20@mhs.unsyiah.ac.id>
2022-05-15 01:31:05 +0700

    update login no fix

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="style/user/login.css">
+</head>
+<body>
+  <div class="container">
+    <div class="container-left">
+      <img src="assets/images/welcome.svg" alt="welcome">
+    </div>
+    <div class="container-right">
+      <h1>Login</h1>
+      </br>
+      <form>
+        <div class="input">
+          <label for="">Username</label>
+          <input type="text">
+        </br>
+        </div>        
+        <div class="input">
+          <label for="">Password</label>
+          <input type="text">
+        </br>
+        </div>
+        <button>Masuk</button>
+      </form> 
+      <div class="reg">
+      <h3>Belum punya akun? <a href="daftar">Daftar</a></h3>
+    </div>
+    </div>
+  </div>
+</body>
+</html>
