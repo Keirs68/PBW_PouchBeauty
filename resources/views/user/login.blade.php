7d7e8c1fba1d44a5770631929a06f473fddc9fe1
Putri Maulida <PUTRI.M20@mhs.unsyiah.ac.id>
2022-05-15 09:37:57 +0700

    fix tampilan halaman login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -9,27 +9,20 @@
 <body>
   <div class="container">
     <div class="container-left">
-      <img src="assets/images/welcome.svg" alt="welcome">
+      <img src="assets/images/wlcome.svg" alt="welcome">
     </div>
     <div class="container-right">
       <h1>Login</h1>
-      </br>
-      <form>
-        <div class="input">
-          <label for="">Username</label>
-          <input type="text">
-        </br>
-        </div>        
-        <div class="input">
-          <label for="">Password</label>
-          <input type="text">
-        </br>
-        </div>
-        <button>Masuk</button>
-      </form> 
-      <div class="reg">
-      <h3>Belum punya akun? <a href="daftar">Daftar</a></h3>
-    </div>
+        <form>
+            <label for="">Username</label><br>
+            <input type="text"><br>
+            <label for="">Password</label><br>
+            <input type="password"><br>
+               <div class="reg">
+                  <h3>Belum Punya Akun? <a href="sign up">Sign up</a></h3><br>
+                </div>
+            <center><button>NEXT</button></center>
+        </form> 
     </div>
   </div>
 </body>
