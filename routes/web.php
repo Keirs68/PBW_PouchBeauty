73ed30d909f4bb298b0881dc5f03d0d4813d14f3
Putri Maulida <PUTRI.M20@mhs.unsyiah.ac.id>
2022-05-15 09:54:16 +0700

    membuat halaman home

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
  
 Route::get('/login', function () {
     return view('user/login');
