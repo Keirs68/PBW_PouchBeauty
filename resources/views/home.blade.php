239db20f4d6abaea28aa94a1bbb3009cdda6ed9b
Putri Maulida <PUTRI.M20@mhs.unsyiah.ac.id>
2022-05-19 12:43:11 +0700

    non fix halaman home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <link rel="stylesheet" href="style/home.css"/>
+</head>
+<body>
+    <section>
+        <!---- Header ---->
+        <!--- logo --->
+        <img class="img-logo" src="assets/images/logo.jpeg" alt="logo">
+        <div class="header">
+            <h1>POUCHBEAUTY</h1>
+            <h5>by Shipoo</h5>
+            <a href="#"><img class="login" src=""></a>
+        </div>
+
+        <!---- navigation bar ---->
+        <div class="nav">
+            <!--- menu --->
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Skin Check</a></li>
+                <li><a href="#">Analyze Product</a></li>
+                <li><a href="#">Skindiary</a></li>
+            </ul>
+        </div>
+
+        <div class="
+
+    
+</body>
+</html>
