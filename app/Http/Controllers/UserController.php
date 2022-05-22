9eaa5ba41c1bad57c7046bce44fe334c755098c8
Putri Maulida <PUTRI.M20@mhs.unsyiah.ac.id>
2022-05-22 23:41:00 +0700

    new add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,19 +6,25 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    public function index()
+    {
+        return view('home');
+    }
+    
     public function login()
     {
-        return view('login',[]);
+        return view('login');
     }
 
     public function signup()
     {
-        return view('signup',[]);
+        return view('signup');
     }
 
     public function profil()
     {
-        return view('profil',[]);
+        return view('profil');
     }
 
 }
--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,31 +8,39 @@
     <title>Sign Up</title>
     <link rel="stylesheet" href="style/user/signup.css">
 </head>
+
 <body>
     <div class="container">
         <h1>Sign Up</h1>
-        <form>
+        <form action="/signup" method="POST">
+            @csrf
             <div class="form-group">
-            <label for="">Username</label><br>
-            <input type="text"><br>
+                <label for="">Name</label><br>
+                <input type="text" name="name" required><br>
             </div>
             <div class="form-group">
-            <label for="">Password</label><br>
-            <input type="password"><br>
+                <label for="">Username</label><br>
+                <input type="text" name="username" required><br>
             </div>
             <div class="form-group">
-            <label for="">Konfirmasi Password</label><br>
-            <input type="password"><br>
+                <label for="">Password</label><br>
+                <input type="password" name="password" required><br>
             </div>
-        </form>
+            <div class="form-group">
+                <label for="">Konfirmasi Password</label><br>
+                <input type="password" name="confirmPassword" required><br>
+            </div>
 
-        <div class="teks">
-            <h1>Or</h1>
-        </div>
-        <div  class="icons">
-            
-        </div>
-        <button type="submit" class="registerbtn">Next</button></right>
+
+            <div class="teks">
+                <h1>Or</h1>
+            </div>
+            <div class="icons">
+
+            </div>
+            <button type="submit" class="registerbtn">submit</button>
+        </form>
     </div>
 </body>
+
 </html>
