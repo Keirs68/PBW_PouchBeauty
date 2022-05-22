a14e982f1eb76b790a997803fb1287c2c95c71f6
Syifa 'Azhima <azhima@mhs.unsyiah.ac.id>
2022-05-22 20:25:17 +0700

    hasil search

---
+++ resources/views/user/hasil-searching.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hasil-searching</title>
+    <link rel="stylesheet" href="style/user/hasil-searching.css">
+</head>
+<body>
+    <form>
+    <input class="search" type="text" placeholder="Cari..." required>	
+    <input class="button" type="button" value="Cari">		
+    </form>
+    
+</body>
+</html>
